04fcc4ed31f0827d5a03e9458eb52bbc8fe7b882
kabeer <bewarekiller7@gmail.com>
2024-02-26 17:27:29 +0500

    Assigment

---
+++ app.js
@@ -0,0 +1,43 @@
+var vra = "numbers, $ and _ . For example : $my_1stVariable"
+document.write("Variables names can only contain ,"+vra)
+
+var rav = "$ or _ . For example : $name, _ name or name"
+document.write("<br>"+"Variable must begin with letter, "+rav )
+
+var ops = "sensitive"
+document.write("<br>"+"Variables names are case "+ops)
+
+var $ = "JS keywords"
+document.write("<br>"+"Variables names should not be "+$)
+
+
+var a = 3
+var b = 5
+document.write("<br>"+"Sum of "+a+" and "+b+" is ",a+b)
+
+var a = 3
+var b = 5
+document.write("<br>"+"Multiplication of "+a+" and "+b+" is ",a*b )
+
+var a = 3
+var b = 5
+document.write("<br>"+"Subtraction of "+a+" and "+b+" is ",a-b )
+
+
+var a = 5
+var b = 6
+var c = 13
+var d = 12
+var e = 0
+
+document.write("<br>"+"<br>"+"Initial value : "+a+"<br>"+"Value after increament is "+b+"<br>"+"Value after addition is : "+c+"<br>"+"Value after decreament is : "+d+"<br>"+"The remainder is : " +e )
+
+
+
+var price = 600
+document.write("<br>"+"<br>"+"<br>"+"Total cost to buy 5 tickets is ",price*5)
+
+
+var a = 4
+document.write("<br>"+"<br>"+"<br>"+"<br>"+"<h1>Table of 4</h1>"+"<br>"+"<br>"+"4 x 1 = ",a*1 +"<br>"+"4 x 2 = ",a*2 +"<br>"+"4 x 3 = ",a*3 +"<br>"+"4 x 4 = ",a*4 +"<br>"+"4 x 5 = ",a*5 +"<br>"+"4 x 6 = ",a*6 +"<br>"+"4 x 7 = ",a*7 +"<br>"+"4 x 8 = ",a*8 +"<br>"+"4 x 9 = ",a*9 +"<br>"+"4 x 10 = ",a*10)
+
